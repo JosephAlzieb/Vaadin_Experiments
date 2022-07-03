3af6eba8d57c1399d1e51b700346c831bd2bb315
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-03 22:53:27 +0200

    Person Form und Person Daten in einem HorizontalLayout anzeigen

--- Experiment_1/src/main/java/com/example/experiment_1/views/MainView.java
+++ Experiment_1/src/main/java/com/example/experiment_1/views/MainView.java
@@ -1,18 +1,23 @@
 package com.example.experiment_1.views;
 
 
+import com.example.experiment_1.layouts.PersonDataLayout;
 import com.example.experiment_1.layouts.PersonFormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
-public class MainView extends VerticalLayout {
+public class MainView extends HorizontalLayout {
+
 
   public MainView(){
 
     PersonFormLayout form = new PersonFormLayout();
 
-    add(form);
+    PersonDataLayout data = new PersonDataLayout();
+
+    addAndExpand(form,data);
   }
 
 }
