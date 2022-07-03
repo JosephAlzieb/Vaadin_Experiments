c69018fa4eeff5f2040f635a65880c12ea2ab1ba
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-03 23:19:48 +0200

    PersonenService übergeben

--- Experiment_1/src/main/java/com/example/experiment_1/views/MainView.java
+++ Experiment_1/src/main/java/com/example/experiment_1/views/MainView.java
@@ -3,8 +3,8 @@ package com.example.experiment_1.views;
 
 import com.example.experiment_1.layouts.PersonDataLayout;
 import com.example.experiment_1.layouts.PersonFormLayout;
+import com.example.experiment_1.services.PersonService;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
@@ -13,9 +13,9 @@ public class MainView extends HorizontalLayout {
 
   public MainView(){
 
-    PersonFormLayout form = new PersonFormLayout();
+    PersonFormLayout form = new PersonFormLayout(new PersonService());
 
-    PersonDataLayout data = new PersonDataLayout();
+    PersonDataLayout data = new PersonDataLayout(new PersonService());
 
     setPadding(true);
 
