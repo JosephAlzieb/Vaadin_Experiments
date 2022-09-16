2ae5eb1f82f082f33083514e04f42a7be9c639d4
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-16 22:51:52 +0200

    crud-ui

---
+++ Experiment_5/src/main/java/com/example/experiment_5/ui/AdminView.java
@@ -0,0 +1,28 @@
+package com.example.experiment_5.ui;
+
+import com.example.experiment_5.backend.Book;
+import com.example.experiment_5.backend.BookService;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+import javax.annotation.security.RolesAllowed;
+import org.vaadin.crudui.crud.impl.GridCrud;
+
+@Route("admin")
+//@RolesAllowed("ADMIN")
+public class AdminView extends VerticalLayout {
+
+  public AdminView(BookService service) {
+    var crud = new GridCrud<>(Book.class, service);
+    crud.getGrid().setColumns("title", "published", "rating");
+    crud.getCrudFormFactory().setVisibleProperties("title", "published", "rating");
+    crud.setAddOperationVisible(false);
+//    crud.getCrudLayout().addToolbarComponent(new RouterLink("New book", NewView.class));
+
+    add(
+        new H1("Admin view"),
+        crud
+    );
+  }
+}
