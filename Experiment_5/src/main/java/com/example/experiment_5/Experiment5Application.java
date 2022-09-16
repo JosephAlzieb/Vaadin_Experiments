67b9ab22298c9c8a2c3ce9be60ee5c8d15a7c0ae
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-16 23:59:08 +0200

    security disabled

--- Experiment_5/src/main/java/com/example/experiment_5/Experiment5Application.java
+++ Experiment_5/src/main/java/com/example/experiment_5/Experiment5Application.java
@@ -3,11 +3,32 @@ package com.example.experiment_5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Experiment5Application {
+public class Experiment5Application{
 
   public static void main(String[] args) {
     SpringApplication.run(Experiment5Application.class, args);
   }
 
+//  @Override
+//  @Bean
+//  protected UserDetailsService userDetailsService() {
+//    return new InMemoryUserDetailsManager(
+//        User.withUsername("Joe")
+//            .password("{noop}p")
+//            .roles("ADMIN")
+//            .build(),
+//        User.withUsername("Mar")
+//            .password("{noop}p")
+//            .roles("ADMIN")
+//            .build()
+//    );
+//  }
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    super.configure(http);
+//    setLoginView(http, LoginView.class);
+//  }
 }
